79d3960bc319e98237482248973fef8e6bf798c5
filipv152 <106246032+filipv152@users.noreply.github.com>
2022-06-10 15:44:59 +0200

    Update matrix.h

--- Projektni Zadatak/matrix.h
+++ Projektni Zadatak/matrix.h
@@ -8,21 +8,21 @@ typedef struct artikl {
 	char proizvodac[40];
 	int id;
 	int kolicina;
-	int tezinaUGramima;
+	int masaUGramima;
 	char gluten[3];
 	char secer[3];
 	int br;
 }ARTIKL;
 
 void inicijalizacija(FILE*);
-void dodajArtikl(const char* const);
-void* citanjeArtikala(const char* const);
+void dodajArtikl(const char* const, ARTIKL*);
+void* citanjeArtikala(const char*);
 void ispisivanjeArtikala(const ARTIKL* const);
 void* pretrazivanjeArtikala(ARTIKL* const);
 int izlazIzPrograma(ARTIKL*);
 void brisanjeArtikala(ARTIKL** const, const ARTIKL* const, const char* const);
 void brisanjeDatoteke(const char*);
 int izbornik(const char* const);
-void promjenaPreferenci(ARTIKL*, const char* const);
+void promjenaKarakteristikaArikla(ARTIKL*, const char* const);
 
 #endif
